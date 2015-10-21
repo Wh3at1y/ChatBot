1ff4b8ab527599e187579a050fe99352b3e9130e
Sam Montoya <smon7958@740s-pl-118.local>
2015-10-21 12:16:52 -0600

    Added to Repo

---
+++ src/chat/controller/ChatbotRunner.java
@@ -0,0 +1,13 @@
+package chat.controller;
+
+/**
+ * Starts the Chatbot program.
+ * @author smon7958
+ * @version1.0 10/20/15
+ */
+
+public class ChatbotRunner
+{
+	public static void main(String[]args)
+	{	ChatbotController startController = new ChatbotController(); startController.start(); }
+}
