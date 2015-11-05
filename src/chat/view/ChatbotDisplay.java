3a64a84b6ed85de77c0c0931605d2dafcd0d5a67
Sam Montoya <smon7958@740s-pl-118.local>
2015-11-05 12:33:16 -0700

    Added the constuctor

--- src/chat/view/ChatbotDisplay.java
+++ src/chat/view/ChatbotDisplay.java
@@ -1,15 +1,26 @@
 package chat.view;
 
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 
 /**
  * Grabs user inputs and displays text to the screen.
  * @author Sam Montoya
- * @version 1.0 10/21/15
+ * @version 1.1 10/21/15 : Added the icon location
  */
 
 public class ChatbotDisplay
 {
+	private String windowMessage;
+	private ImageIcon chatIcon;
+	
+	public ChatbotDisplay()
+	{
+		windowMessage = "Stupid Chatbot message";
+		chatIcon = new ImageIcon(getClass().getResource("images/WorldIcon.png"));
+		
+	}
+	
 	/**
 	 * Displays text and asks for a user input.
 	 * @param displayText : Shows what is printed to the screen.
